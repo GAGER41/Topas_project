4f311cb63affeb8bd72afd7da1eb9a3d37eeabd3
Gabrielle Germain <57801267+GAGER41@users.noreply.github.com>
2023-11-02 19:50:41 -0400

    thursday night work

--- Figure6_12/figure6_12.py
+++ Figure6_12/figure6_12.py
@@ -2,12 +2,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
-path = r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Figure6_12\Dose_water_phantom_122MeV.csv"
+# paths = [r"",
+#          r"",
+#          r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\Dose_water_phantom_122MeV_14.csv"]
+path = r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\Dose_water_phantom_230MeV_15.csv"
 data = np.loadtxt(path, skiprows=9, delimiter=', ', usecols=(2, 3, 4))
 # print(data)
-
-plt.plot(data[:, 0]*0.333, data[:, 1])
+field = 100 # cm2
+history = 1000
+fluence = history/field
+plt.plot(data[:, 0]*0.35, data[:, 1]/1.602176565e-13/1000/fluence)
 plt.xlabel('depth (cm)')
-plt.ylabel("Energy deposition (Gy)")
+plt.ylabel("Energy deposition (MeV/g)")
 plt.show()
 
+# the energy is divided byt the history number so we have the energy depoosition per proton
+
+# spread of 0.65 cm
+# cuttof of 5. cm
+# bigger world: 50cm x 50cm x 50cm
