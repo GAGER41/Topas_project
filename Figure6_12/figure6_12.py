4869728ae60ac8513562a80a527e7b66d93745ce
Gabrielle Germain <57801267+GAGER41@users.noreply.github.com>
2023-11-06 15:42:31 -0500

    tests with 1000000 histories

--- Figure6_12/figure6_12.py
+++ Figure6_12/figure6_12.py
@@ -2,16 +2,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
+
+MeV_to_joule = 1.602176565e-13 # J/MeV
+
 # paths = [r"",
 #          r"",
 #          r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\Dose_water_phantom_122MeV_14.csv"]
-path = r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\6_12_protons_230MeV_rect_10000h.csv"
+path = r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\6_12_protons_183MeV_broad.csv"
 data = np.loadtxt(path, skiprows=9, delimiter=', ', usecols=(2, 3, 4))
 # print(data)
 field = 100 # cm2
-history = 10000
+history = 1000000
 fluence = history/field
-plt.plot(data[:, 0]*0.1, data[:, 1]/1.602176565e-13/1000/10000)
+bin = data[:, 0]
+dose = data[:, 1]
+std = data[:, 2]
+bin_thick = 0.1
+
+std_rel = (np.nanmean(std/dose))*100
+std_max = (np.nanmax(std/dose))*100
+print("mean: ", std_rel)
+print("max: ", std_max)
+
+plt.plot(bin*bin_thick, dose/MeV_to_joule/1000/fluence)
 plt.xlabel('depth (cm)')
 plt.ylabel("Energy deposition (MeV/g)")
 plt.show()
@@ -22,4 +35,6 @@ plt.show()
 # beamspread of 0.5 mm for narrow, 6.5 mm for broad
 # bigger world necessary: 50cm x 50cm x 50cm
 
-#### test branch
+# voxel volume: 2x2x2 = 8mm^3
+#               5x5x5 = 125 mm^3
+# code cloé: 62.5 mm^3
