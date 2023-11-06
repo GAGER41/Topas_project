459ee81052dbd2b5e9355833bac9a76ae171e4b4
Gabrielle Germain <57801267+GAGER41@users.noreply.github.com>
2023-11-06 11:12:50 -0500

    6 nov

--- Figure6_12/figure6_12.py
+++ Figure6_12/figure6_12.py
@@ -5,19 +5,19 @@ import pandas as pd
 # paths = [r"",
 #          r"",
 #          r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\Dose_water_phantom_122MeV_14.csv"]
-path = r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\Dose_water_phantom_230MeV_15.csv"
+path = r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\6_12_protons_230MeV_rect_10000h.csv"
 data = np.loadtxt(path, skiprows=9, delimiter=', ', usecols=(2, 3, 4))
 # print(data)
 field = 100 # cm2
-history = 1000
+history = 10000
 fluence = history/field
-plt.plot(data[:, 0]*0.35, data[:, 1]/1.602176565e-13/1000/fluence)
+plt.plot(data[:, 0]*0.1, data[:, 1]/1.602176565e-13/1000/10000)
 plt.xlabel('depth (cm)')
 plt.ylabel("Energy deposition (MeV/g)")
 plt.show()
 
-# the energy is divided byt the history number so we have the energy depoosition per proton
+# the energy is divided by the history number so we have the energy deposition per proton, and the surface of the beam
 
-# spread of 0.65 cm
-# cuttof of 5. cm
-# bigger world: 50cm x 50cm x 50cm
+# beamcuttof of 5. cm
+# beamspread of 0.5 mm for narrow, 6.5 mm for broad
+# bigger world necessary: 50cm x 50cm x 50cm
