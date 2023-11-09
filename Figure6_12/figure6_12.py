b02cdb19a1a7ba744306ad32e523f31f5248419e
Gabrielle Germain <57801267+GAGER41@users.noreply.github.com>
2023-11-09 14:55:51 -0500

    2 attempts

--- Figure6_12/figure6_12.py
+++ Figure6_12/figure6_12.py
@@ -5,29 +5,39 @@ import pandas as pd
 
 MeV_to_joule = 1.602176565e-13 # J/MeV
 
-# paths = [r"",
-#          r"",
-#          r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\Dose_water_phantom_122MeV_14.csv"]
-# path = r"C:\Users\gabri\Documents\Université\Maitrise\Session 1\physique_radiations\Devoir\Topas_project\Figure6_12\6_12_carbon_430MeV_broad.csv"
-path = r"Topas_project\Figure6_12\6_12_carbon_350MeV_broad.csv"
-data = np.loadtxt(path, skiprows=9, delimiter=', ', usecols=(2, 3, 4))
-# print(data)
-field = 100 # cm2
-history = 10000
-fluence = history/field
-bin = data[:, 0]
-dose = data[:, 1]
-std = data[:, 2]
-bin_thick = 0.1
+# paths = [r"Topas_project\Figure6_12\6_13_carbon_430MeV_i75_5.csv", 
+#         r"Topas_project\Figure6_12\6_13_carbon_430MeV_i1000.csv", 
+#         r"Topas_project\Figure6_12\6_13_carbon_430MeV_i500.csv", 
+#         r"Topas_project\Figure6_12\6_13_carbon_430MeV_i250.csv", 
+#         r"Topas_project\Figure6_12\6_13_carbon_430MeV_i125.csv", 
+#         r"Topas_project\Figure6_12\6_13_carbon_430MeV_i150.csv"]
+paths =[r"Topas_project\Figure6_12\6_12_carbon_220MeV_broad_1.csv",
+        r"Topas_project\Figure6_12\6_12_carbon_430MeV_broad_2.csv"]
+energy =[75, 1000, 500, 250, 125, 150]
+i = 0
+for path in paths:
+    data = np.loadtxt(path, skiprows=9, delimiter=', ', usecols=(2, 3, 4))
+    # print(data)
+    field = 1 # cm2
+    history = 1000
+    fluence = history/field
+    bin = data[:, 0]
+    dose = data[:, 1]
+    std = data[:, 2]
+    bin_thick = 0.1
 
-std_rel = (np.nanmean(std/dose))*100
-std_max = (np.nanmax(std/dose))*100
-print("mean: ", std_rel)
-print("max: ", std_max)
+    # std_rel = (np.nanmean(std/dose))*100
+    # std_max = (np.nanmax(std/dose))*100
+    # print("mean: ", std_rel)
+    # print("max: ", std_max)
 
-plt.plot(bin*bin_thick, dose/MeV_to_joule/1000/fluence)
+    plt.plot(bin*bin_thick, dose/MeV_to_joule/1000/fluence, label=energy[i])
+    i+=1
+
 plt.xlabel('depth (cm)')
 plt.ylabel("Energy deposition (MeV/g)")
+# plt.xlim(29, 31.5)
+# plt.legend()
 plt.show()
 
 # the energy is divided by the history number so we have the energy deposition per proton, and the surface of the beam
@@ -39,3 +49,13 @@ plt.show()
 # voxel volume: 2x2x2 = 8mm^3
 #               5x5x5 = 125 mm^3
 # code cloé: 62.5 mm^3
+
+
+# essayé pour carbone: 
+    # i:Sc/DoseAtPhantom/EBin = 100
+    # d:Sc/DoseAtPhantom/EBinMin = 0 MeV
+    # d:Sc/DoseAtPhantom/EBinMax = 2, 10 MeV, aucune différence
+
+
+# essayé pour i-value:
+    # =100, aucune différence
